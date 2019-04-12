746aa1f17d18479b4efa42c92161f925403b8186
Paula Tulis <tulis.paula@gmail.com>
2019-04-12 21:43:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ def roll_call_dwarves(names)
   end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.each do |x|
+    x.upcase
 end
 
 def long_planeteer_calls# code an argument here
